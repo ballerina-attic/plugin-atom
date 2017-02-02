bcac9932573f7b0f47f7aa4dd3e470ee53b00399
ramindu90 <ramindus@wso2.com>
2017-02-02 17:05:38 +0530

    integrating atom parser initial version

---
+++ spec/language-ballerina-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutocompleteXView from '../lib/language-ballerina-view';
+
+describe('AutocompleteXView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
